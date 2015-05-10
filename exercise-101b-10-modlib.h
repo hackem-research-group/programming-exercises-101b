295ccdba1a0ec257d1a227dfe175f69571af21b6
galoget <galoget@users.noreply.github.com>
2015-05-10 01:19:52 -0500

    Update exercise-101b-10-modlib.h

--- exercise-101b-10-modlib.h
+++ exercise-101b-10-modlib.h
@@ -4,18 +4,18 @@
 int main ()
 {
 	int a, b;
-		printf("ingrese el numerador: \n");
-        scanf("%d", &a);
-		printf("\nIngrese el denominador: \n");
-		scanf("%d", &b);
+	printf("ingrese el numerador: \n");
+	scanf("%d", &a);
+	printf("\nIngrese el denominador: \n");
+	scanf("%d", &b);
 
-	if(b==0){
-	printf("\nEl modulo es cero");
+	if(b==0)
+	{
+	    printf("\nEl modulo es cero");
 	}
-	else {
+	else
+	{
             printf("\nEL Modulo es:%d/%d=%d",a,b,a/b);
-
 	}
 	return 0;
-
 }

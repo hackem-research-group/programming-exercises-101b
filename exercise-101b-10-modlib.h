fe010550a1eb83f184f0f8c97468cd1ba741eb88
farcox <faro12.1994@hotmail.com>
2015-05-10 00:57:51 -0500

    added exercise

---
+++ exercise-101b-10-modlib.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+	int a, b;
+		printf("ingrese el numerador: \n");
+        scanf("%d", &a);
+		printf("\nIngrese el denominador: \n");
+		scanf("%d", &b);
+
+	if(b==0){
+	printf("\nEl modulo es cero");
+	}
+	else {
+            printf("\nEL Modulo es:%d/%d=%d",a,b,a/b);
+
+	}
+	return 0;
+
+}
